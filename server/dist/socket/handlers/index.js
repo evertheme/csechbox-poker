b156f806d795c61f745cff674e46c77bd65b7376
evertheme <github@evertheme.com>
2026-04-15 11:09:38 -0500

    handler index

--- server/dist/socket/handlers/index.js
+++ server/dist/socket/handlers/index.js
@@ -1,8 +1,5 @@
 import { registerGameHandlers } from "./game-handler.js";
 import { registerRoomHandlers } from "./room-handler.js";
-export { registerGameHandlers } from "./game-handler.js";
-export { registerRoomHandlers } from "./room-handler.js";
-/** Attach room + game socket handlers for each new connection. */
 export function registerHandlers(io) {
     io.on("connection", (socket) => {
         registerRoomHandlers(io, socket);
--- server/src/socket/handlers/index.ts
+++ server/src/socket/handlers/index.ts
@@ -1,13 +1,9 @@
-import type { Server } from "socket.io";
+import type { Server, Socket } from "socket.io";
 import { registerGameHandlers } from "./game-handler.js";
 import { registerRoomHandlers } from "./room-handler.js";
 
-export { registerGameHandlers } from "./game-handler.js";
-export { registerRoomHandlers } from "./room-handler.js";
-
-/** Attach room + game socket handlers for each new connection. */
 export function registerHandlers(io: Server): void {
-  io.on("connection", (socket) => {
+  io.on("connection", (socket: Socket) => {
     registerRoomHandlers(io, socket);
     registerGameHandlers(io, socket);
   });
